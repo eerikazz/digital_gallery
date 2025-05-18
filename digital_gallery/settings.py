d0d50233bcf958ef45c6cb8e319066ab2c05a476
Erikas J <erikas.privat3043@gmail.com>
2025-05-18 22:29:08 +0200

    Update settings.py

--- digital_gallery/settings.py
+++ digital_gallery/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure--npg$wo*h(b_dd85lx+-bbe=^((^9cfb1%@00uyx^ez@(i8@^j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "digital-gallery-9caxz.ondigitalocean.app",
+    "localhost",          # keep local runserver happy
+    "127.0.0.1",
+]
 
 
 # Application definition
