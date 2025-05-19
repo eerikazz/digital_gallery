058f407ef98be140c61a9ef0a1ada6ffbcb7b420
Erikas J <erikas.privat3043@gmail.com>
2025-05-19 12:18:18 +0200

    Update settings.py

--- digital_gallery/settings.py
+++ digital_gallery/settings.py
@@ -67,10 +67,23 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# allow sending cookies in a third-party context
+CSRF_COOKIE_SAMESITE   = 'None'
+CSRF_COOKIE_SECURE     = True
+
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE   = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "'self'",                 # you can still embed your own pages if needed
+    "https://gather.town",
+    "https://app.gather.town",
 ]
 
 ROOT_URLCONF = 'digital_gallery.urls'
